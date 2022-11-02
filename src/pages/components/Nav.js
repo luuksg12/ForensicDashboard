38a5235a6490e414188831dc4a2a699c0c4ac7fc
Luuk 's-Gravendijk <47536174+luuksg12@users.noreply.github.com>
2022-11-02 09:43:08 +0100

    navbar update general layout

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -6,11 +6,14 @@ function Nav (){
 
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                <a class="nav-item nav-link" href="/Sessions">Sessies</a>
-                <a class="nav-item nav-link" href="/sessioninfo">Sessies</a>
-                <a class="nav-item nav-link" href="/Map">Kaart</a>
+            <div class="collapse navbar-collapse container justify-content-between" id="navbarNavAltMarkup">
+                <div class="navbar-nav navpad">
+                    <a class="nav-item nav-link" href="/Sessions">Sessies</a>
+                    <a class="nav-item nav-link" href="/sessioninfo">Sessies</a>
+                    <a class="nav-item nav-link" href="/Map">Kaart</a>
+                </div>
+                <div class="navbar-nav logout">
+                <a class="nav-item nav-link" href="/Login">Logout</a>
                 </div>
             </div>
         </nav>
