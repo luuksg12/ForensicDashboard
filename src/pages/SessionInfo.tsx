ec2730d664a931cc03ff4215a158c15a7c85c61c
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-11-06 16:21:36 +0100

    feat: dynamic state changes for sessions and corresponding participants

--- src/pages/SessionInfo.tsx
+++ src/pages/SessionInfo.tsx
@@ -1,38 +1,87 @@
-import React, {useState} from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { json, Link, useLocation } from 'react-router-dom';
 import Nav from "./components/Nav";
 import "../styling/SessionInfo.css"
+import { Session } from './../models/session.model'
+import { User } from './../models/user.model'
 
-function SessionInfo (){
-  const [SessionInfo, setSessionInfo] = useState(
-    //placeholder data
-      { id: "1", date: "10/4/2022", time:"10:23", duration: "12:21", player1: "Jan Joost", player2: "John Doe", map: "cabin", situation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat" },
-  )
 
-  //mapping data and storing in evidence variable 
-    return (
-      <div className="container-fluid p-0">
-        <Nav/>
-        <div className="politie"></div>
-        <div className="card container">
-            <img className="card-img-top" src="..." alt="Card image cap"/>
-            <div className="card-body">
-              <h5 className="card-title">{SessionInfo.map}</h5>
-              <p className="card-text">{SessionInfo.situation}</p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">{SessionInfo.date} - {SessionInfo.time}</li>
-              <li className="list-group-item">Duur : {SessionInfo.duration}</li>
-              <li className="list-group-item">{SessionInfo.player1} / {SessionInfo.player2}</li>
-              <li className="list-group-item"><Link to="/map" style={{ textDecoration: 'none' }}><input className="input-background form-control form-control-lg" type="submit" value="zie map"></input></Link></li>
-            </ul>
-            <div className="card-body">
-              <a href="#" className="card-link">Card link</a>
-              <a href="#" className="card-link">Another link</a>
-            </div>
-          </div>
+function SessionInfo() {
+  const location = useLocation();
+  const sessionId = location.state?.data;
+
+  const [SessionInfo, setSessionInfo] = useState<Session>()
+  const [Supervisors, setSupervisors] = useState<User[]>([])
+  const [Trainees, setTrainees] = useState<User[]>([])
+
+  const SUPERVISOR = 1
+  const TRAINEE = 0
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (
+        await fetch(
+          `http://145.24.222.175/simulation/session?sessionId=${sessionId}`
+        )
+      ).json();
+      await setSessionInfo(data);
+    };
+
+    dataFetch();
+  }, [sessionId]);
+
+  useEffect(() => {
+    if (!!SessionInfo?.participants) {
+      const allUsers: Promise<User>[] = SessionInfo?.participants.map(async (userJSON) => {
+        return await (
+          await fetch(
+            `http://145.24.222.175/simulation/user?id=${userJSON['userId']}`
+          )
+        ).json();
+      })
+      const userBase = Promise.all(allUsers)
+      userBase.then((users) => {
+        setSupervisors(users.filter(user => user.role === SUPERVISOR))
+        setTrainees(users.filter(user => user.role === TRAINEE))
+      })
+    }
+
+  }, [SessionInfo?.participants]);
+
+  const trainees = Trainees.map((trainee, index) =>
+    <li key={index} className="list-group-item">Trainee {index+1}: {trainee.firstname} {trainee.lastname}</li>
+  );
+
+  const supervisors = Supervisors.map((supervisor, index) =>
+    <li key={index} className="list-group-item">Supervisor {index+1}: {supervisor.firstname} {supervisor.lastname}</li>
+  );
+
+  return (
+    <div className="container-fluid p-0">
+      <Nav />
+      <div className="politie"></div>
+      <div className="card container">
+        <img className="card-img-top" src="..." alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{SessionInfo?.id}</h5>
+          <p className="card-text">{SessionInfo?.description}</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          {supervisors}
+          {trainees}
+          <li className="list-group-item">Session description: {SessionInfo?.description}</li>
+          <li className="list-group-item">Scene: {SessionInfo?.scene?.name}</li>
+          <li className="list-group-item">Start tijd: {SessionInfo?.startTime}</li>
+          <li className="list-group-item">Stop tijd: {(!!SessionInfo?.stopTime) ? SessionInfo?.stopTime : 'nog niet gestopt'}</li>
+          <li className="list-group-item"><Link to="/map" style={{ textDecoration: 'none' }}><input className="input-background form-control form-control-lg" type="submit" value="zie map"></input></Link></li>
+        </ul>
+        <div className="card-body">
+          <a href="#" className="card-link">Card link</a>
+          <a href="#" className="card-link">Another link</a>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default SessionInfo;
--- src/pages/Sessions.tsx
+++ src/pages/Sessions.tsx
@@ -1,29 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import { SessionModel } from "../models/session.model";
 import { SessionQuery } from "../querys/SessionQuery";
 import Nav from "./components/Nav";
 import "../styling/Sessions.css"
+import SessionInfo from "./SessionInfo";
 
-function Sessions (){
-  const [selectedItem, setSelectedItem] = useState()
+function Sessions() {
+  const [selectedItem, setSelectedItem] = useState({id:"none"})
 
   const [logoList, setlogoList] = useState(
     //placeholder data
     [
-      { id: "1", name:"Logo" },
-      { id: "2", name:"Logo" },
+      { id: "1", name: "Logo" },
+      { id: "2", name: "Logo" },
     ]
   )
 
   const listOfSessions: SessionModel[] = [
     {
-        id: "",
-        description: "",
-        superVisor: "",
-        events: [],
-        date: new Date(),
-        duration: new Date(),
+      id: "",
+      description: "",
+      superVisor: "",
+      events: [],
+      date: new Date(),
+      duration: new Date(),
     }
   ];
 
@@ -38,10 +39,14 @@ function Sessions (){
 
   // const [theArray, setTheArray] = useState(friendsArray);
 
-  const sessions = SessionList.map((item, index) =>
-    <li key={index} onClick={() => handleSelection(item, index)} style={{cursor : 'pointer'}}>
-      <div className={"session"}>{item.description }</div>
-    </li>
+  const sessions = SessionList.map((item, index) => {
+    return (
+      <li key={index} onClick={() => handleSelection(item, index)} style={{ cursor: 'pointer' }}>
+        <div className={"session"}>{item.description}</div>
+      </li>
+    )
+  }
+    
   );
 
   const logos = logoList.map((value) =>
@@ -50,11 +55,13 @@ function Sessions (){
     </div>
   );
 
+
+
   useEffect(() => {
     fetchData();
   }, []);
 
-  async function fetchData(){
+  async function fetchData() {
     var fetchedSessions = await SessionQuery.GetSessions()
 
     console.log(fetchedSessions)
@@ -62,34 +69,37 @@ function Sessions (){
     setSessionList(fetchedSessions)
   }
 
-  function handleSelection(e: any, index: number){
+
+  function handleSelection(e: any, index: number) {
     console.log(index)
+    
     var setActive = Array.from(document.getElementsByClassName('session'))
-    for(let item of setActive){
+    for (let item of setActive) {
       item.classList.remove('selected')
     }
     setActive[index].classList.toggle('selected')
+    console.log(setActive[index])
     setSelectedItem(e)
   }
   //mapping data and storing in evidence variable 
-    return (
-      <div className="container-fluid p-0">
-        <Nav/>
-        <div className="politie"></div>
-          <div className="row-9 main d-flex align-items-center justify-content-center">
-            <div className="d-flex flex-column sessionHolder">
-              <ul onChange={(e) => handleSelection} style={{ listStyle: 'none', padding: 0 }}>
-                {sessions}
-              </ul>
-              
-              <Link to="/sessionInfo" style={{ textDecoration: 'none' }}><input className="input-background form-control form-control-lg" type="submit" value="Selecteer"></input></Link>
-            </div>
-          </div>
-          <div className="bottombar row-3 container">
-            {logos}
-          </div>
+  return (
+    <div className="container-fluid p-0">
+      <Nav />
+      <div className="politie"></div>
+      <div className="row-9 main d-flex align-items-center justify-content-center">
+        <div className="d-flex flex-column sessionHolder">
+          <ul onChange={(e) => handleSelection} style={{ listStyle: 'none', padding: 0 }}>
+            {sessions}
+          </ul>
+          <Link to="/sessionInfo" state={{data: selectedItem.id}} style={{ textDecoration: 'none' }}><input className="input-background form-control form-control-lg" type="submit" value="Selecteer"></input></Link>
+          
+        </div>
       </div>
-    )
-  }
+      <div className="bottombar row-3 container">
+        {logos}
+      </div>
+    </div>
+  )
+}
 
 export default Sessions;
