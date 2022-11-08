8587cb2ff6d2e6b0d4803f25fa9948ae3a4ee37f
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-11-08 16:56:39 +0100

    feat: correctly presents evidences on the map

--- src/pages/components/Map.tsx
+++ src/pages/components/Map.tsx
@@ -2,22 +2,39 @@ import React, { useState } from "react";
 import "../../styling/Map.css"
 import { Session } from './../../models/session.model'
 import { EvidenceType } from "./../../models/evidence.model"
+
 interface SessionProp {
   session: Session
 }
 
 export function Map(props: SessionProp) {
   const session = props.session
-  console.log(props.session.scene.evidences)
-  const scaleX = session.scene.scaleX;
-  const scaleY = session.scene.scaleY;
+
+  // translatedZvalue and translatedXvalue are temporary fixes for a specific map.
+  // will 100% break whenever a new map is being used with a different layout.
+
+  const translatedZvalue = (zValue: number) => { return (((zValue / 23.8) * 100) / 104) * 100; }
+  const translatedXvalue = (xValue: number) => {
+    let result;
+    if(xValue <= 0){
+      result = 60 - ((xValue / - 16) * 100);
+    } else {
+      result = (((xValue / 3.37) * 100) / 110) * 100;
+    }
+    return result;
+  }
+
   const evidences = props.session.scene.evidences
   const [evidenceList, setEvidenceList] = useState(evidences)
 
   //mapping data and storing in evidence variable 
-  const evidence = evidenceList.map((value, index) =>
-    <div key={value.id} className={"evidence " + value} style={{ left: ((value.x * 100)) + 'px', top: (385 - (value.y * 100)) + "px" }}>{EvidenceType[value.type]}: {value.type}</div>
-  );
+  const evidence = evidenceList.map((value, index) => {
+    console.log(value.x)
+    console.log(translatedXvalue(value.x))
+    return <div key={value.id} className={"evidence " + value} style={{ left: `${translatedZvalue(value.z)}%`, top: `${translatedXvalue(value.x)}%` }}>{EvidenceType[value.type]}: {value.type}</div>
+
+  });
+
   return (
     <div className="container">
       <div className="row">
