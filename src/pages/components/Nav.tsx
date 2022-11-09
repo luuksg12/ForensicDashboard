22d9506863248285ce4447e9e692ba354521dd51
Luuk 's-Gravendijk <47536174+luuksg12@users.noreply.github.com>
2022-11-09 23:40:29 +0100

    Police and nav fix

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -6,13 +6,15 @@ function Nav (){
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="collapse navbar-collapse container justify-content-between" id="navbarNavAltMarkup">
-                <div className="navbar-nav navpad">
-                    <Link to="/Sessions" className="Navlink"><p>Sessies</p></Link>
-                    <Link to="/Sessioninfo" className="Navlink"><p>Informatie</p></Link>
-                </div>
-                <div className="navbar-nav logout">
-                    <Link to="/" className="Navlink"><i className="fa fa-sign-out" aria-hidden="true"></i></Link>
+            <div className="container">
+                <div className="collapse navbar-collapse container justify-content-between" id="navbarNavAltMarkup">
+                    <div className="navbar-nav navpad">
+                        <Link to="/Sessions" className="Navlink"><p>Sessies</p></Link>
+                        <Link to="/Sessioninfo" className="Navlink"><p>Informatie</p></Link>
+                    </div>
+                    <div className="navbar-nav logout">
+                        <Link to="/" className="Navlink"><i className="fa fa-sign-out" aria-hidden="true"></i></Link>
+                    </div>
                 </div>
             </div>
         </nav>
